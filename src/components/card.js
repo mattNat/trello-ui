1028a7aed2d54ebef545848a3cec507a7f07bf8f
System <none>
2017-02-01 16:19:03 +0000

    Checkpoint

---
+++ src/components/card.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Card() {
+    const text  = 'Example card';
+    return (
+        <div className="card">
+            {text}
+        </div>
+    );
+};
+
