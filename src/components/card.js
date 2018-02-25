1864884ff8a694e4fb190360eb01068511fd649b
Glitch (adjoining-athlete) <none>
2018-02-24 22:06:08 -0800

    init project

--- src/components/card.js
+++ src/components/card.js
@@ -2,11 +2,15 @@ import React from 'react';
 
 import './card.css';
 
-// avoid arrow functions for 
+// avoid arrow functions for declaring components
 export default function Card() {
     const text  = 'Example card';
     return (
+        // JSX syntax 
         <div className="card">
+            // will access text variable
+            // can also put {a + b}
+            // {'abc'.toUpperCase()}
             {text}
         </div>
     );
